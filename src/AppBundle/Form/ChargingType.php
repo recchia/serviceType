7ea571ec2caa35aae6490da232a09f6e289ddcf5
Piero Recchia <precchia_ext@dlatv.net>
2016-09-30 15:46:35 -0300

    Setted validation for number

--- src/AppBundle/Form/ChargingType.php
+++ src/AppBundle/Form/ChargingType.php
@@ -14,13 +14,21 @@ use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
 
 class ChargingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('phone', TextType::class, ['label' => 'Cuenta'])
+            ->add('phone', TextType::class, [
+                'label' => 'Cuenta',
+                'constraints' => [
+                    new NotBlank(['message' => 'Debe ingresar cuenta']),
+                    new Type(['type' => 'digit', 'message' => 'El valor {{ value }} no es un {{ type }} v&aacute;lido'])
+                ]
+            ])
             ->add('service_type', HiddenType::class)
             ->add('send', ButtonType::class, ['label' => 'Enviar'])
             ;
